ecf8c36b6327dade2a2686b0477f3087c6b9d333
Stefan König <skonig@transip.nl>
2015-11-10 00:28:56 +0100

    Added mqtt username + password authentication support

--- config.js
+++ config.js
@@ -10,5 +10,7 @@ config.hue.username   = process.env.HUE_USERNAME   || 'unknown';
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || 'light';
 config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'localhost';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
+config.mqtt.username  = process.env.MQTT_USERNAME  || '';
+config.mqtt.password  = process.env.MQTT_PASSWORD  || '';
 
 module.exports = config;
